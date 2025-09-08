f3b53a4a86495f0faea7a2431e29b440a07f7990
Enraging <116453008+requireNonNull@users.noreply.github.com>
2025-09-09 00:08:38 +0200

    Update HorseLife.lua

--- HorseLife.lua
+++ HorseLife.lua
@@ -1,5 +1,5 @@
 -- HorseLife CoinFarm (All-in-One, Instant TP)
-local VERSION = "v0.0.5"
+local VERSION = "v0.0.5 fix 001"
 local CHANGELOG = "<+> Added +20 Y offset for horse sitting <+>"
 
 local Players = game:GetService("Players")
@@ -32,11 +32,8 @@ local function tpTo(character, targetPos)
 	local humanoid = character:FindFirstChildOfClass("Humanoid")
 	if not hrp or not humanoid then return end
 
-	local yPos = targetPos.Y
-		yPos = result.Position.Y + 20
-
 	-- Insta-TP
-	hrp.CFrame = CFrame.new(Vector3.new(targetPos.X, yPos, targetPos.Z))
+	hrp.CFrame = CFrame.new(Vector3.new(targetPos.X, yPos + 20, targetPos.Z))
 end
 
 function CoinFarmer.Start(statusLabel)
