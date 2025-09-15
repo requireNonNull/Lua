351cf8f210077d69f7917997e2c02754e25532ac
Enraging <116453008+requireNonNull@users.noreply.github.com>
2025-09-15 14:17:46 +0200

    Update HorseLifeUIStatus.lua

--- HorseLifeUIStatus.lua
+++ HorseLifeUIStatus.lua
@@ -2,7 +2,7 @@ local VERSION = "v6.7 alien17"
 local NAME = "🦄 Farmy"
 
 -- Version Check
-local LATEST_VERSION = "v6.7 alien17" -- change this when new version comes
+local LATEST_VERSION = "v6.7 alien17"
 local versionMessage = ""
 if VERSION ~= LATEST_VERSION then
     versionMessage = "⚠️ Outdated: " .. VERSION .. " | Latest: " .. LATEST_VERSION
@@ -10,12 +10,13 @@ else
     versionMessage = "✅ Up to date: " .. VERSION
 end
 
--- Game Data
+-- Game Data (HorseLife)
 return {
     Name = "HorseLife",
+    PlaceId = 15696848933,
     Status = "<+> Exploit Working <+>",
     LastCheckedDate = "2025-09-15",
     ExploitName = NAME,
     ExploitVersion = VERSION,
-    VersionInfo = versionMessage -- add this so UI can show it under title
+    VersionInfo = versionMessage
 }
